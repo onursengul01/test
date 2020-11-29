3cdb5184c809e127df840c42ced7e6bec7f3e153
onursengul01 <57270214+onursengul01@users.noreply.github.com>
2020-11-29 13:19:09 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import os.path
+import sys
+path = '/usr/bin/TheFatRat'
+gitRepo = 'https://github.com/Screetsec/TheFatRat'
+
+if os.getuid() != 0:
+	print("Sorry, this script requires sudo privledges")
+	sys.exit()
+
+#if os.path.exists(path):
+ 	#print("settolkit is ready to use")
+#else:
+	#while True:
+print("TheFatRat is not found in the system...")
+option = input("Would you like to install TheFatRat? 'y' or 'n': ")
+if option == "y":
+	os.system(f"git clone {gitRepo} {path}" )
+	os.chdir(f"cd {path}/")
+	os.system("sudo bash setup.sh")
+
+
+if option == "n":
+	print("bye")
+	
